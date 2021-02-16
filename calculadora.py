e0c40f10fcdf94fc79a6d60cb742b6e662163445
elliot.silva <elliotportugal@gmail.com>
2021-02-16 14:14:39 -0300

    finalização do código

---
+++ calculadora.py
@@ -0,0 +1,53 @@
+calculadora = {
+    "soma": lambda a, b: int(a + b),
+    "subtracao": lambda a, b: a - b,
+    "multiplicacao": lambda a, b: a * b,
+    "divisao": lambda a, b: a / b, 
+}
+
+soma = calculadora["soma"]
+subtracao = calculadora["subtracao"]
+multiplicacao = calculadora["multiplicacao"]
+divisao = calculadora["divisao"]
+
+rep = "s"
+
+while rep == "s":
+    print("=====Bem-vindo à calculadora Python!=====")
+    
+    opcao = int(input("Selecione a operação desejada: \n1- Soma \n 2- Subtração \n 3- Multiplicação \n 4- Divisão \n 5- Sair\n"))
+    if opcao == 1:
+        a = int(input("Selecione o primeiro número: "))
+        b = int(input("Selecione o segundo número: "))
+        resultado = soma(a,b)
+        print(resultado)
+    
+    elif opcao == 2:
+        a = int(input("Selecione o primeiro número: "))
+        b = int(input("Selecione o segundo número: "))
+        resultado = subtracao(a,b)
+        print(resultado)
+
+    elif opcao == 3:
+        a = int(input("Selecione o primeiro número: "))
+        b = int(input("Selecione o segundo número: "))
+        resultado = multiplicacao(a,b)
+        print(resultado)
+
+    elif opcao == 4:
+        a = int(input("Selecione o primeiro número: "))
+        b = int(input("Selecione o segundo número: "))
+        resultado = divisao(a,b)
+        print(resultado)
+
+    elif opcao == 5:
+        print("Você está saindo")
+
+    elif opcao > 5:
+        print("A opção invalida")
+        
+    rep = str(input("Deseja realizar outra operação? [S]im [N]ão: "))
+    while rep != 's' and rep != 'n':
+        rep = str(input("Entre com um valor valido: "))
+
+print("Obrigado, volte sempre!")
